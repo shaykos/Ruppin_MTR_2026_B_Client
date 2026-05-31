e7b0daccd96c3f995b733ad0e3a4e7ae20f82eee
Shay Abraham <shaykos@gmail.com>
2026-05-31 10:25:29 +0300

    הוספה של רנדומליות בהצבת הקלפים

--- class 7/HW/Q1/scrtips/functions.js
+++ class 7/HW/Q1/scrtips/functions.js
@@ -1,32 +1,42 @@
 
 export function createCards() {
-    let imgs = document.querySelectorAll('#cards img');
-    imgs.forEach((img, index) => {
-        switch (index + 1) {
-            case 1:
-            case 12:
-                img.src = "./assets/images/1.png";
-                break;
-            case 2:
-            case 11:
-                img.src = "./assets/images/2.png";
-                break;
-            case 3:
-            case 10:
-                img.src = "./assets/images/3.png";
-                break;
-            case 4:
-            case 9:
-                img.src = "./assets/images/4.png";
-                break;
-            case 5:
-            case 8:
-                img.src = "./assets/images/5.png";
-                break;
-            case 6:
-            case 7:
-                img.src = "./assets/images/6.png";
-                break;
+    let images = document.querySelectorAll('#cards img');
+    let imagesCount = new Array(6).fill(0);
+
+    while (imagesCount.some((value) => value != 2)) {
+
+        let index = getRandomNumber(0, 11);
+        while (!checkIfAvailable(images, index)) {
+            index = getRandomNumber(0, 11);
         }
-    });
+
+        let img = getRandomNumber(1, 6);
+        while (imagesCount[img - 1] == 2) {
+            img = getRandomNumber(1, 6);
+        }
+
+        images[index].dataset.card = `./assets/images/${img}.png`;
+        imagesCount[img - 1]++;
+    }
 }
+
+function getRandomNumber(min, max) {
+    return Math.floor(Math.random() * (max - min + 1) + min);
+}
+
+function checkIfAvailable(arr, index) {
+    // step 1 -> return arr[index].src.endsWith('back.gif');
+    /*
+    dataset.card ==> data-card
+    קיימת html אנחנו בודקים אם התכונה הזו ב 
+    undefined במידה ולא נקבל
+    */
+    return arr[index].dataset.card == undefined
+}
+
+export function flip(event) {
+    let element = event.target; // האלמנט שהפעיל את האירוע
+
+    element.src = element.dataset.card;
+}
+
--- class 7/HW/Q1/scrtips/main.js
+++ class 7/HW/Q1/scrtips/main.js
@@ -1,3 +1,9 @@
-import { createCards } from './functions.js'
+import { createCards, flip } from './functions.js'
 
+//סידור הקלפים באופן רנדומלי
 createCards();
+
+//קישור אירוע לחיצה לכל תמונה
+document.querySelectorAll('#cards img').forEach((element) => {
+    element.addEventListener('click', flip);
+});
